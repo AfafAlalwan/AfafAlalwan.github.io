8856ef59e4514f98e59eaf5c726b111a1580ad0b
Afaf <afafalwan02@gmail.com>
2023-02-07 15:40:56 +0300

    Clear template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://AfafAlalwan.github.io',
-	//integrations: [mdx(), sitemap()],
 });
